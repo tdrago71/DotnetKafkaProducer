2ff469d66bf1a5a75583e6ec4eae65e89f745233
Ravi <dev.raviraghav93@gmail.com>
2023-03-25 03:10:30 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using KafkaProducer;
+
+Console.WriteLine("Hello!, This is my Kafka Producer Application");
+ProduceMessage produceMessage = new ProduceMessage();
+produceMessage.CreateMessage().Wait();
